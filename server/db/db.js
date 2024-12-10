888e95929e2af88590c51ad936e6979ecb1a594f
CJung14 <chris,cj.jung@gmail.com>
2024-12-10 14:26:07 -0500

    initiated app, established connection to db

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+const host = process.env.HOST
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`,
+const db = new Sequelize(pkg.name, username, password, 
+  {
+  host: host,
+  dialect: 'postgres'
+  }, 
   {
     logging: false,
   }
